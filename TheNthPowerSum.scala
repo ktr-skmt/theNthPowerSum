37c8f246d2cb8a848a0850c01092da18e98a8103
Kohtaro Sakamoto <ktr.skmt@gmail.com>
2012-12-07 21:10:37 +0900

    second

--- TheNthPowerSum.scala
+++ TheNthPowerSum.scala
@@ -7,7 +7,7 @@ object TheNthPowerSum {
   def calc(x : Int, n : Int, p : Int) : Unit =
     if (n >= 0) for (i <- 0 to x) calc(i, n - 1, p)
     else y += math.pow(x, p)
-  def printExecutionTime(proc : => Unit) = {
+  def printExecutionTime(proc : => Unit) {
     val start = System.currentTimeMillis
     proc
     println("[total " + (System.currentTimeMillis - start) + "ms]")
